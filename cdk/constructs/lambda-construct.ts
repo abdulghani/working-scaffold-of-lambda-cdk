90206e13e23ca651568179af83806c173f7b61fc
Ghani <contact.abdulghani@gmail.com>
2024-06-25 14:08:11 +0700

    chore: setup email transporter and env

--- cdk/constructs/lambda-construct.ts
+++ cdk/constructs/lambda-construct.ts
@@ -30,7 +30,11 @@ export class LambdaConstruct extends Construct {
         DB_PORT: process.env.DB_PORT || "",
         DB_USER: process.env.DB_USER || "",
         DB_PASSWORD: process.env.DB_PASSWORD || "",
-        DB_NAME: process.env.DB_NAME || ""
+        DB_NAME: process.env.DB_NAME || "",
+        SMTP_HOST: process.env.SMTP_HOST || "",
+        SMTP_PORT: process.env.SMTP_PORT || "",
+        SMTP_USER: process.env.SMTP_USER || "",
+        SMTP_PASSWORD: process.env.SMTP_PASSWORD || ""
       }
     });
   }
