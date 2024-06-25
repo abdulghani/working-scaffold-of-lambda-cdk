2cec624ef08a77828d55b8fe20a0c774ee781e7b
Ghani <contact.abdulghani@gmail.com>
2024-06-25 16:26:47 +0700

    chore: center queue layout for bigger screen, simplify route handling

--- app/routes/queue.$posId.tsx
+++ app/routes/queue.$posId.tsx
@@ -67,31 +67,23 @@ export async function action({ request, params }: ActionFunctionArgs) {
   const cookie = await queueCookie.parse(request.headers.get("Cookie"));
   const queue = cookie ? JSON.parse(cookie) : null;
 
-  if (cancel === "true" && queue?.id && posId === queue?.pos_id) {
-    await cancelQueue?.(queue.id as string);
+  if (cancel === "true") {
+    if (queue?.id && posId === queue?.pos_id) {
+      await cancelQueue?.(queue.id as string);
+    }
     throw redirect(`/queue/${params.posId}`, {
-      headers: {
-        "Set-Cookie": await queueCookie.serialize("")
-      }
-    });
-  } else if (cancel === "true") {
-    throw redirect(`/queue/${posId}`, {
       headers: {
         "Set-Cookie": await queueCookie.serialize("")
       }
     });
   }
 
-  const name = form.get("name");
-  const pax = form.get("pax");
-  const phone = form.get("phone");
   const newQueue = await addQueue?.({
-    name,
-    pax,
-    phone,
+    name: form.get("name"),
+    pax: form.get("pax"),
+    phone: form.get("phone"),
     posId
   });
-
   throw redirect(`/queue/${posId}`, {
     headers: {
       "Set-Cookie": await queueCookie.serialize(JSON.stringify(newQueue))
@@ -105,152 +97,161 @@ export default function Queue() {
 
   return (
     <>
-      <Tabs defaultValue="account" className="mt-3 w-[400px]">
-        <TabsList className="sticky top-3 mx-4 grid grid-cols-2">
-          <TabsTrigger value="account">Antrian</TabsTrigger>
-          <TabsTrigger value="password">Antri</TabsTrigger>
-        </TabsList>
-        <TabsContent value="account">
-          <Card className="border-0 shadow-none">
-            <CardHeader>
-              <CardTitle>Antrian {pos.name}</CardTitle>
-              <CardDescription>List antrian restoran</CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-2">
-              <Table>
-                <TableHeader>
-                  <TableRow>
-                    <TableHead>#</TableHead>
-                    <TableHead>Name</TableHead>
-                    <TableHead>PAX</TableHead>
-                    <TableHead className="text-right">Time</TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  {queues.map((q, i) => (
-                    <TableRow
-                      key={q.id}
-                      className={q.id === queue?.id ? "bg-sky-50" : undefined}
-                    >
-                      <TableCell className="font-medium">{i + 1}</TableCell>
-                      <TableCell>{q.name}</TableCell>
-                      <TableCell>{q.pax}</TableCell>
-                      <TableCell className="text-right">
-                        {moment(q.created_at).fromNow()}
-                      </TableCell>
+      <div className="flex w-screen justify-center">
+        <Tabs defaultValue="account" className="mt-3 w-[400px]">
+          <TabsList className="sticky top-3 mx-4 grid grid-cols-2">
+            <TabsTrigger value="account">Antrian</TabsTrigger>
+            <TabsTrigger value="password">Antri</TabsTrigger>
+          </TabsList>
+          <TabsContent value="account">
+            <Card className="border-0 shadow-none">
+              <CardHeader>
+                <CardTitle>Antrian {pos.name}</CardTitle>
+                <CardDescription>List antrian restoran</CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <Table>
+                  <TableHeader>
+                    <TableRow>
+                      <TableHead>#</TableHead>
+                      <TableHead>Name</TableHead>
+                      <TableHead>PAX</TableHead>
+                      <TableHead className="text-right">Time</TableHead>
                     </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </CardContent>
-          </Card>
-        </TabsContent>
-        <TabsContent value="password">
-          <Card className="border-0 shadow-none">
-            {queue ? (
-              <>
-                <CardHeader>
-                  <CardTitle>
-                    Antrian no {queues.findIndex((q) => q.id === queue.id) + 1}
-                  </CardTitle>
-                  <CardDescription>
-                    Anda sudah mengantri atas nama {queue.name} (untuk{" "}
-                    {queue.pax} orang)
-                  </CardDescription>
-                  <CardDescription>
-                    {new Date(queue.created_at).toLocaleString()} (
-                    {moment(queue.created_at).fromNow()})
-                  </CardDescription>
-                </CardHeader>
-                <CardFooter>
-                  <AlertDialog
-                    open={cancelDialog}
-                    onOpenChange={setCancelDialog}
-                  >
-                    <AlertDialogTrigger asChild>
-                      <Button className="w-full" variant="outline">
-                        Batalkan antrian
-                      </Button>
-                    </AlertDialogTrigger>
-                    <AlertDialogContent>
-                      <AlertDialogHeader>
-                        <AlertDialogTitle>Apakah kamu yakin?</AlertDialogTitle>
-                        <AlertDialogDescription>
-                          Antrian akan dibatalkan dan tidak bisa dikembalikan
-                        </AlertDialogDescription>
-                      </AlertDialogHeader>
-                      <Form
-                        method="post"
-                        className="w-full"
-                        onSubmit={() => {
-                          setCancelDialog(false);
-                        }}
+                  </TableHeader>
+                  <TableBody>
+                    {queues.map((q, i) => (
+                      <TableRow
+                        key={q.id}
+                        className={q.id === queue?.id ? "bg-sky-50" : undefined}
                       >
-                        <AlertDialogFooter>
-                          <AlertDialogCancel>Batal</AlertDialogCancel>
-                          <Button type="submit" name="cancel" value={"true"}>
-                            Hapus
-                          </Button>
-                        </AlertDialogFooter>
-                      </Form>
-                    </AlertDialogContent>
-                  </AlertDialog>
-                </CardFooter>
-              </>
-            ) : (
-              <>
-                <CardHeader>
-                  <CardTitle>Antri</CardTitle>
-                  <CardDescription>Masukan data antrian kamu</CardDescription>
-                </CardHeader>
-                <Form method="post">
-                  <CardContent className="space-y-2">
-                    <div className="space-y-1">
-                      <Label htmlFor="name">Nama</Label>
-                      <Input
-                        id="name"
-                        name="name"
-                        type="text"
-                        required
-                        value={queue?.name}
-                        disabled={!!queue}
-                      />
-                    </div>
-                    <div className="space-y-1">
-                      <Label htmlFor="pax">Jumlah orang (PAX)</Label>
-                      <Input
-                        id="pax"
-                        name="pax"
-                        type="number"
-                        inputmode="numeric"
-                        required
-                        value={queue?.pax}
-                        disabled={!!queue}
-                      />
-                    </div>
-                    <div className="space-y-1">
-                      <Label htmlFor="phone">No Handphone</Label>
-                      <Input
-                        id="phone"
-                        name="phone"
-                        type="number"
-                        inputmode="numeric"
-                        value={queue?.phone}
-                        disabled={!!queue}
-                      />
-                    </div>
-                  </CardContent>
+                        <TableCell className="font-medium">{i + 1}</TableCell>
+                        <TableCell>{q.name}</TableCell>
+                        <TableCell>{q.pax}</TableCell>
+                        <TableCell className="text-right">
+                          {moment(q.created_at).fromNow()}
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </CardContent>
+            </Card>
+          </TabsContent>
+          <TabsContent value="password">
+            <Card className="border-0 shadow-none">
+              {queue ? (
+                <>
+                  <CardHeader>
+                    <CardTitle>
+                      Antrian no{" "}
+                      {queues.findIndex((q) => q.id === queue.id) + 1}
+                    </CardTitle>
+                    <CardDescription>
+                      Anda sudah mengantri atas nama {queue.name} (untuk{" "}
+                      {queue.pax} orang)
+                    </CardDescription>
+                    <CardDescription>
+                      {new Date(queue.created_at).toLocaleString()} (
+                      {moment(queue.created_at).fromNow()})
+                    </CardDescription>
+                  </CardHeader>
                   <CardFooter>
-                    <Button className="w-full" type="submit" disabled={!!queue}>
-                      Antri
-                    </Button>
+                    <AlertDialog
+                      open={cancelDialog}
+                      onOpenChange={setCancelDialog}
+                    >
+                      <AlertDialogTrigger asChild>
+                        <Button className="w-full" variant="outline">
+                          Batalkan antrian
+                        </Button>
+                      </AlertDialogTrigger>
+                      <AlertDialogContent>
+                        <AlertDialogHeader>
+                          <AlertDialogTitle>
+                            Apakah kamu yakin?
+                          </AlertDialogTitle>
+                          <AlertDialogDescription>
+                            Antrian akan dibatalkan dan tidak bisa dikembalikan
+                          </AlertDialogDescription>
+                        </AlertDialogHeader>
+                        <Form
+                          method="post"
+                          className="w-full"
+                          onSubmit={() => {
+                            setCancelDialog(false);
+                          }}
+                        >
+                          <AlertDialogFooter>
+                            <AlertDialogCancel>Batal</AlertDialogCancel>
+                            <Button type="submit" name="cancel" value={"true"}>
+                              Hapus
+                            </Button>
+                          </AlertDialogFooter>
+                        </Form>
+                      </AlertDialogContent>
+                    </AlertDialog>
                   </CardFooter>
-                </Form>
-              </>
-            )}
-          </Card>
-        </TabsContent>
-      </Tabs>
+                </>
+              ) : (
+                <>
+                  <CardHeader>
+                    <CardTitle>Antri</CardTitle>
+                    <CardDescription>Masukan data antrian kamu</CardDescription>
+                  </CardHeader>
+                  <Form method="post">
+                    <CardContent className="space-y-2">
+                      <div className="space-y-1">
+                        <Label htmlFor="name">Nama</Label>
+                        <Input
+                          id="name"
+                          name="name"
+                          type="text"
+                          required
+                          value={queue?.name}
+                          disabled={!!queue}
+                        />
+                      </div>
+                      <div className="space-y-1">
+                        <Label htmlFor="pax">Jumlah orang (PAX)</Label>
+                        <Input
+                          id="pax"
+                          name="pax"
+                          type="number"
+                          inputmode="numeric"
+                          required
+                          value={queue?.pax}
+                          disabled={!!queue}
+                        />
+                      </div>
+                      <div className="space-y-1">
+                        <Label htmlFor="phone">No Handphone</Label>
+                        <Input
+                          id="phone"
+                          name="phone"
+                          type="number"
+                          inputmode="numeric"
+                          value={queue?.phone}
+                          disabled={!!queue}
+                        />
+                      </div>
+                    </CardContent>
+                    <CardFooter>
+                      <Button
+                        className="w-full"
+                        type="submit"
+                        disabled={!!queue}
+                      >
+                        Antri
+                      </Button>
+                    </CardFooter>
+                  </Form>
+                </>
+              )}
+            </Card>
+          </TabsContent>
+        </Tabs>
+      </div>
     </>
   );
 }
